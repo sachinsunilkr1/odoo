3c98a4682c4ae841f5ca19f530e2c975297f61b8
sachin <sachinsunilkr1@gmail.com>
2020-12-08 22:04:11 +0530

    second commit commmit

--- pos_product_rating/static/src/js/rating.js
+++ pos_product_rating/static/src/js/rating.js
@@ -1,7 +1,9 @@
 odoo.define('pos_product_rating.rating', function (require) {
 "use strict";
 
-
+    var screens = require('point_of_sale.screens');
+	var core = require('web.core');
+	var QWeb = core.qweb;
     var models = require('point_of_sale.models');
 
 
@@ -9,18 +11,17 @@ odoo.define('pos_product_rating.rating', function (require) {
     //    loading field prodcut ratings to pos
     models.load_fields("product.product", ['product_ratings']);
     console.log("hiii",models)
-    console.log("prodcut")
-//    console.log("product rating",product.product_ratings)
 
-     //    adding the field discount_tag in the receipt
-//    var _super_orderline = models.Orderline.prototype;
-//    models.Orderline = models.Orderline.extend({
-//        export_for_printing: function() {
-//            var line = _super_orderline.export_for_printing.apply(this,arguments);
-//            line.product_ratings = this.get_product().product_ratings;
-//            return line;
-//        },
-//    });
+     var _super_orderline = models.Orderline.prototype;
+        models.Orderline = models.Orderline.extend({
+            export_for_printing: function() {
+                var line = _super_orderline.export_for_printing.apply(this,arguments);
+                line.product_ratings = this.get_product().product_ratings;
+                return line;
+            },
+        });
+
+
 
 
 });
